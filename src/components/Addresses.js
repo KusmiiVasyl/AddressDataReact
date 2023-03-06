876d30943c5f342ebf258178bf618df2ba0b7a41
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2023-03-06 19:40:05 +0200

    Create container Loading... when datas API loading

--- src/components/Addresses.js
+++ src/components/Addresses.js
@@ -8,13 +8,26 @@ export default function Addresses() {
     const ADDRESSES_URL = 'https://my.api.mockaroo.com/address.json?key=c2449c70'
     const [addresses, setAddresses] = useState([])
     const [error, setError] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetch(ADDRESSES_URL)
             .then((result) => result.json())
             .then((addresses) => setAddresses(addresses))
             .catch((error) => setError(error.message))
+            .finally(()=> setIsLoading(false))
     }, [])
+
+    if(isLoading){
+        return (
+            <>
+                <h1>Addresses</h1>
+                <div className="addresses">
+                    <p className="loading">Loading...</p>
+                </div>
+            </>
+        )
+    }
 
     if (error) {
         return (
@@ -38,103 +51,103 @@ export default function Addresses() {
     )
 }
 
-// const addresses = [
-//     {
-//         "id": 1,
-//         "country": "Russia",
-//         "city": "Okhotsk",
-//         "street": "9072 Utah Plaza",
-//         "isHomeAddress": true
-//     }, {
-//         "id": 2,
-//         "country": "Paraguay",
-//         "city": "Caacupé",
-//         "street": "32030 Memorial Alley",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 3,
-//         "country": "Cuba",
-//         "city": "Primero de Enero",
-//         "street": "25456 Donald Road",
-//         "isHomeAddress": true
-//     }, {
-//         "id": 4,
-//         "country": "Bosnia and Herzegovina",
-//         "city": "Kladanj",
-//         "street": "9 Thompson Place",
-//         "isHomeAddress": true
-//     }, {
-//         "id": 5,
-//         "country": "Zambia",
-//         "city": "Nyimba",
-//         "street": "26753 Oak Trail",
-//         "isHomeAddress": true
-//     }, {
-//         "id": 6,
-//         "country": "China",
-//         "city": "Taiyanghe",
-//         "street": "4 Nancy Lane",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 7,
-//         "country": "China",
-//         "city": "Lishu",
-//         "street": "88 Bultman Alley",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 8,
-//         "country": "Sweden",
-//         "city": "Edsbyn",
-//         "street": "0192 Division Parkway",
-//         "isHomeAddress": true
-//     }, {
-//         "id": 9,
-//         "country": "Philippines",
-//         "city": "Cambanugoy",
-//         "street": "478 Haas Street",
-//         "isHomeAddress": true
-//     }, {
-//         "id": 10,
-//         "country": "China",
-//         "city": "Dongshui",
-//         "street": "5 Norway Maple Place",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 11,
-//         "country": "Egypt",
-//         "city": "Būsh",
-//         "street": "28804 Killdeer Circle",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 12,
-//         "country": "United States",
-//         "city": "Anniston",
-//         "street": "8848 Boyd Circle",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 13,
-//         "country": "Hungary",
-//         "city": "Budapest",
-//         "street": "94 Ridgeway Park",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 14,
-//         "country": "China",
-//         "city": "Shiyang",
-//         "street": "4 Sage Road",
-//         "isHomeAddress": true
-//     }, {
-//         "id": 15,
-//         "country": "Uruguay",
-//         "city": "Barra de Carrasco",
-//         "street": "59146 Little Fleur Lane",
-//         "isHomeAddress": false
-//     }, {
-//         "id": 16,
-//         "country": "China",
-//         "city": "Shijiao",
-//         "street": "1 Algoma Way",
-//         "isHomeAddress": false
-//     }]
+const addresses = [
+    {
+        "id": 1,
+        "country": "Russia",
+        "city": "Okhotsk",
+        "street": "9072 Utah Plaza",
+        "isHomeAddress": true
+    }, {
+        "id": 2,
+        "country": "Paraguay",
+        "city": "Caacupé",
+        "street": "32030 Memorial Alley",
+        "isHomeAddress": false
+    }, {
+        "id": 3,
+        "country": "Cuba",
+        "city": "Primero de Enero",
+        "street": "25456 Donald Road",
+        "isHomeAddress": true
+    }, {
+        "id": 4,
+        "country": "Bosnia and Herzegovina",
+        "city": "Kladanj",
+        "street": "9 Thompson Place",
+        "isHomeAddress": true
+    }, {
+        "id": 5,
+        "country": "Zambia",
+        "city": "Nyimba",
+        "street": "26753 Oak Trail",
+        "isHomeAddress": true
+    }, {
+        "id": 6,
+        "country": "China",
+        "city": "Taiyanghe",
+        "street": "4 Nancy Lane",
+        "isHomeAddress": false
+    }, {
+        "id": 7,
+        "country": "China",
+        "city": "Lishu",
+        "street": "88 Bultman Alley",
+        "isHomeAddress": false
+    }, {
+        "id": 8,
+        "country": "Sweden",
+        "city": "Edsbyn",
+        "street": "0192 Division Parkway",
+        "isHomeAddress": true
+    }, {
+        "id": 9,
+        "country": "Philippines",
+        "city": "Cambanugoy",
+        "street": "478 Haas Street",
+        "isHomeAddress": true
+    }, {
+        "id": 10,
+        "country": "China",
+        "city": "Dongshui",
+        "street": "5 Norway Maple Place",
+        "isHomeAddress": false
+    }, {
+        "id": 11,
+        "country": "Egypt",
+        "city": "Būsh",
+        "street": "28804 Killdeer Circle",
+        "isHomeAddress": false
+    }, {
+        "id": 12,
+        "country": "United States",
+        "city": "Anniston",
+        "street": "8848 Boyd Circle",
+        "isHomeAddress": false
+    }, {
+        "id": 13,
+        "country": "Hungary",
+        "city": "Budapest",
+        "street": "94 Ridgeway Park",
+        "isHomeAddress": false
+    }, {
+        "id": 14,
+        "country": "China",
+        "city": "Shiyang",
+        "street": "4 Sage Road",
+        "isHomeAddress": true
+    }, {
+        "id": 15,
+        "country": "Uruguay",
+        "city": "Barra de Carrasco",
+        "street": "59146 Little Fleur Lane",
+        "isHomeAddress": false
+    }, {
+        "id": 16,
+        "country": "China",
+        "city": "Shijiao",
+        "street": "1 Algoma Way",
+        "isHomeAddress": false
+    }]
 
 
